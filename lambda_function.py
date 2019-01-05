4aad536a49c7e8ab58ebdefd977de4b9f68d47bd
Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>
2019-01-05 21:07:57 +0530

    Builders and helper function implemented in lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,4 +1,55 @@
-import json
+def build_PlainSpeech(body):
+    speech = {}
+    speech['type'] = 'PlainText'
+    speech['text'] = body
+    return speech
+
+
+def build_response(message, session_attributes={}):
+    response = {}
+    response['version'] = '1.0'
+    response['sessionAttributes'] = session_attributes
+    response['response'] = message
+    return response
+
+
+def build_SimpleCard(title, body):
+    card = {}
+    card['type'] = 'Simple'
+    card['title'] = title
+    card['content'] = body
+    return card
+
+
+def conversation(title, body, session_attributes):
+    speechlet = {}
+    speechlet['outputSpeech'] = build_PlainSpeech(body)
+    speechlet['card'] = build_SimpleCard(title, body)
+    speechlet['shouldEndSession'] = False
+    return build_response(speechlet, session_attributes=session_attributes)
+
+
+def statement(title, body):
+    speechlet = {}
+    speechlet['outputSpeech'] = build_PlainSpeech(body)
+    speechlet['card'] = build_SimpleCard(title, body)
+    speechlet['shouldEndSession'] = True
+    return build_response(speechlet)
+
+
+def continue_dialog():
+    message = {}
+    message['shouldEndSession'] = False
+    message['directives'] = [{'type': 'Dialog.Delegate'}]
+    return build_response(message)
+
+def on_launch(event, context):
+    json_object=statement("Maths Formula", "Welcome to Maths formula created by Rajdeep Roy Chowdhury")
+    return json_object
 
 def lambda_handler(event, context):
-    print('This skill was just invoked by Rajdeep')
+    return on_launch(event, context)
+
+if __name__=='__main__':
+    print('The json object that is returned is...')
+    print(lambda_handler('checking','json response in local'))
