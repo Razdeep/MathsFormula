a41f7b53e54fab146572c3d6e4dc2fd7929813b0
Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>
2019-01-06 11:11:59 +0530

    intentRouter setup

--- lambda_function.py
+++ lambda_function.py
@@ -1,54 +1,76 @@
-def build_PlainSpeech(body):
+import math
+
+def sine(value):
+    return round(math.sin(value*(22/7)/180))
+def cosine(value):
+    return round(math.cos(value*(22/7)/180))
+def tangent(value):
+    return round(math.tan(value*(22/7)/180))
+
+def buildPlainSpeech(body):
     speech = {}
     speech['type'] = 'PlainText'
     speech['text'] = body
     return speech
 
-
-def build_response(message, session_attributes={}):
+def buildResponse(message, session_attributes={}):
     response = {}
     response['version'] = '1.0'
     response['sessionAttributes'] = session_attributes
     response['response'] = message
     return response
 
-
-def build_SimpleCard(title, body):
+def buildSimpleCard(title, body):
     card = {}
     card['type'] = 'Simple'
     card['title'] = title
     card['content'] = body
     return card
 
-
-def conversation(title, body, session_attributes):
-    speechlet = {}
-    speechlet['outputSpeech'] = build_PlainSpeech(body)
-    speechlet['card'] = build_SimpleCard(title, body)
-    speechlet['shouldEndSession'] = False
-    return build_response(speechlet, session_attributes=session_attributes)
-
+# Intent Handlers
+def cancel_intent():
+    return statement("CancelIntent", "You want to cancel")	#don't use CancelIntent as title it causes code reference error during certification 
+def help_intent():
+    return statement("CancelIntent", "You want help")		#same here don't use CancelIntent
+def stop_intent():
+    return statement("StopIntent", "You want to stop")		#here also don't use StopIntent
+# Intent Handlers end here
 
 def statement(title, body):
     speechlet = {}
-    speechlet['outputSpeech'] = build_PlainSpeech(body)
-    speechlet['card'] = build_SimpleCard(title, body)
+    speechlet['outputSpeech'] = buildPlainSpeech(body)
+    speechlet['card'] = buildSimpleCard(title, body)
     speechlet['shouldEndSession'] = True
-    return build_response(speechlet)
+    return buildResponse(speechlet)
 
+def statementAndListen(title, body):
+    speechlet = {}
+    speechlet['outputSpeech'] = buildPlainSpeech(body)
+    speechlet['card'] = buildSimpleCard(title, body)
+    speechlet['shouldEndSession'] = False
+    return buildResponse(speechlet)
 
-def continue_dialog():
-    message = {}
-    message['shouldEndSession'] = False
-    message['directives'] = [{'type': 'Dialog.Delegate'}]
-    return build_response(message)
+def onLaunch(event, context):
+    return statementAndListen("Maths Formula", "Welcome to Maths formula created by Rajdeep Roy Chowdhury")
 
-def on_launch(event, context):
-    json_object=statement("Maths Formula", "Welcome to Maths formula created by Rajdeep Roy Chowdhury")
-    return json_object
+def intentRouter(event, context):
+    intent = event['request']['intent']['name']
+    # Custom Intents
+    if intent == 'SineIntent':
+        return 'This would return Sine Json object'
+    # Required Intents
+    if intent == "AMAZON.CancelIntent":
+        return cancel_intent()
+    if intent == "AMAZON.HelpIntent":
+        return help_intent()
+    if intent == "AMAZON.StopIntent":
+        return stop_intent()
 
 def lambda_handler(event, context):
-    return on_launch(event, context)
+    if event['request']['type'] == "LaunchRequest":
+        return onLaunch(event, context)
+    elif event['request']['type'] == "IntentRequest":
+        return intentRouter(event, context)
 
 if __name__=='__main__':
     print('The json object that is returned is...')
