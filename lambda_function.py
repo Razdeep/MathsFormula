fb3350ad2684d97741fa4e49d18246eb453dae23
Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>
2019-01-07 14:04:33 +0530

    More information for the user added

--- lambda_function.py
+++ lambda_function.py
@@ -9,6 +9,12 @@ def cosine(value):
 def tangent(value):
     '''Returns tangent of an angle (in degrees)'''
     return math.tan(value*(22/7)/180)
+def factorial(n):
+    '''Returns factorial of n'''
+    result = 1
+    for i in range(1,n+1):
+        result=result*i
+    return result
 def sumOfNaturalNumbers(n):
     '''Return sum of n natural numbers'''
     return n*(n+1)//2
@@ -58,7 +64,8 @@ def sayAndListen(title, body):
     return buildResponse(speechlet)
 
 def onLaunch(event, context):
-    return sayAndListen("Maths Formula", "Welcome to Maths formula")
+    text= "Welcome to Maths formula, you can ask me to compute any mathematical function by saying something like, 'What is the sine of 45 degree?'"
+    return sayAndListen("Maths Formula",text)
 
 def intentRouter(event, context):
     intent = event['request']['intent']['name']
@@ -67,33 +74,36 @@ def intentRouter(event, context):
         value=event['request']['intent']['slots']['value']['value']
         value=float(value)
         text='Haha! The sine of '+str(value)+' is '+str(sine(value))
-        return sayAndListen('Answer',text)
+        return say('Answer',text)
     elif intent == 'CosineIntent':
         value=event['request']['intent']['slots']['value']['value']
         value=float(value)
         text='Haha! The cosine of '+str(value)+' is '+str(cosine(value))
-        return sayAndListen('Answer',text)
+        return say('Answer',text)
     elif intent == 'TangentIntent':
         value=event['request']['intent']['slots']['value']['value']
         value=float(value)
         text='Haha! The tangent of '+str(value)+' is '+str(tangent(value))
-        return sayAndListen('Answer',text)
+        return say('Answer',text)
     elif intent == 'CosecIntent':
         value=event['request']['intent']['slots']['value']['value']
         value=float(value)
         text='Haha! The cosec of '+str(value)+' is '+str(1/sine(value))
-        return sayAndListen('Answer',text)
+        return say('Answer',text)
     elif intent ==  'SecantIntent':
         value=event['request']['intent']['slots']['value']['value']
         value=float(value)
         text='Haha! The secant of '+str(value)+' is '+str(1/cosine(value))
-        return sayAndListen('Answer',text)
+        return say('Answer',text)
     elif intent == 'CotIntent':
         value=event['request']['intent']['slots']['value']['value']
         value=float(value)
         text='Haha! The cot of '+str(value)+' is '+str(1/tangent(value))
+        return say('Answer',text)
+    #elif intent == 'PowerIntent':
+    # @TODO: Implement
     elif intent == 'AboutIntent':
-        return sayAndListen('Developer','I was created by Rajdeep Roy Chowdhury')
+        return say('Developer','I was created by Rajdeep Roy Chowdhury')
     # Required Intents
     elif intent == "AMAZON.CancelIntent":
         return cancel_intent()
